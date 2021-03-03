00e06eea1dc00ec98b81ad600f486fbe0b92c9d8
ptjb <ptjb@users.noreply.github.com>
2021-03-03 12:06:54 +0000

    Got tests working

--- test/test_comparison.py
+++ test/test_comparison.py
@@ -1,22 +1,27 @@
 import unittest
-from mathics.core.expression import Integer, Rational, Real, Complex
+from mathics.core.expression import Integer, Rational, Real, Complex, SymbolTrue, SymbolFalse
 from mathics.builtin.comparison import Equal
+from mathics.core.definitions import Definitions
+from mathics.core.evaluation import Evaluation
 
 class ComparisonTest(unittest.TestCase):
+    def setUp(self):
+        self.evaluation = Evaluation(Definitions(True))
+
     def testEqual(self):
-        self.equal_instance = Equal()
         specifications = (
             _EqualityTestSpecification(Integer(1),Integer(1),True),
             _EqualityTestSpecification(Integer(1),Integer(2),False)
         )
         for specification in specifications:
-            self.assertEqual(self.equal_instance.do_compare(specification.left, specification.right), specification.result)
+            temp = Equal(specification.left, specification.right).evaluate(self.evaluation)
+            self.assertEqual(temp, specification.result)
 
 class _EqualityTestSpecification:
     def __init__(self, left, right, result):
         self.left = left
         self.right = right
-        self.result = result
+        self.result = SymbolTrue if result else SymbolFalse
 
 if __name__ == "__main__":
     unittest.main()
