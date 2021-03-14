9daaf8ed7454db56efe15a6d530f36f44a3bfea8
ptjb <ptjb@users.noreply.github.com>
2021-03-14 18:31:20 +0000

    WIP

--- test/test_comparison.py
+++ test/test_comparison.py
@@ -11,7 +11,27 @@ class ComparisonTest(unittest.TestCase):
     def testEqual(self):
         specifications = (
             _EqualityTestSpecification(Integer(1),Integer(1),True),
-            _EqualityTestSpecification(Integer(1),Integer(2),False)
+            _EqualityTestSpecification(Integer(1),Integer(2),False),
+            _EqualityTestSpecification(Rational(1,1),Integer(1),True),
+            _EqualityTestSpecification(Rational(1,1),Rational(2,2),True),
+            _EqualityTestSpecification(Rational(1,1),Rational(1,2),False),
+            _EqualityTestSpecification(Rational(1,1),Rational(2,1),False),
+            _EqualityTestSpecification(Rational(1,2),Integer(2),False),
+            _EqualityTestSpecification(Real(1.0),Integer(1),True),
+            _EqualityTestSpecification(Real(1.0),Rational(1,1),True),
+            _EqualityTestSpecification(Real(1.0),Real(1.0),True),
+            _EqualityTestSpecification(Real(1.0),Real(1.0),True),
+            _EqualityTestSpecification(Real(1.0),Real(2.0),False),
+            _EqualityTestSpecification(Complex(1.0,0.0),Integer(1),True),
+            _EqualityTestSpecification(Complex(1.0,0.0),Rational(1),True),
+            _EqualityTestSpecification(Complex(1.0,0.0),Real(1.0),True),
+            _EqualityTestSpecification(Complex(0.0,1.0),Integer(1),False),
+            _EqualityTestSpecification(Complex(0.0,1.0),Rational(1,1),False),
+            _EqualityTestSpecification(Complex(0.0,1.0),Real(1.0),False),
+            _EqualityTestSpecification(Complex(1.0,1.0),Complex(1.0,1.0),True),
+            _EqualityTestSpecification(Complex(1.0,1.0),Complex(1.0,2.0),False),
+            _EqualityTestSpecification(Complex(1.0,1.0),Complex(2.0,1.0),False),
+            _EqualityTestSpecification(Complex(1.0,1.0),Complex(2.0,3.0),False)
         )
         for specification in specifications:
             temp = Equal(specification.left, specification.right).evaluate(self.evaluation)
