5bb591360f32b2c222b687d2a09f8e2408188ba7
ptjb <ptjb@users.noreply.github.com>
2021-03-15 17:02:11 +0000

    Expanded numeric comparison tests

--- test/test_compare.py
+++ test/test_compare.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .helper import check_evaluation
+from test.helper import check_evaluation
 
 def test_compare():
     for str_expr, str_expected in (
@@ -19,5 +19,60 @@ def test_compare():
             "I + 5 == I",
             "False",
         ),
+        (
+            "0 == 0",
+            "True"
+        ),
+        (
+            "0 == 1",
+            "False"
+        ),
+        (
+            "1/2 == 1/2",
+            "True"
+        ),
+        (
+            "1/2 == 2/4",
+            "True"
+        ),
+        (
+            "2/2 == 1",
+            "True"
+        ),
+        (
+            "1/2 == 2/3",
+            "False"
+        ),
+        (
+            "0.0 == 0.0",
+            "True"
+        ),
+        (
+            "0.0 == 0",
+            "True"
+        ),
+        (
+            "0.0 == 0/2",
+            "True"
+        ),
+        (
+            "0.0 == 1",
+            "False"
+        ),
+        (
+            "0.0 == 1.0",
+            "False"
+        ),
+        (
+            "0.0 == 1.0*I",
+            "False"
+        ),
+        (
+            "0 == 1.0*I",
+            "False"
+        ),
     ):
         check_evaluation(str_expr, str_expected)
+
+if __name__ == "__main__":
+    test_compare()
